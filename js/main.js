077aee07d890bbbdc1d8f25fdc34178720450c12
FranCastro <franciscocast@gmail.com>
2022-07-09 11:55:43 -0300

    Version 09.07 Segunda entrega

--- js/main.js
+++ js/main.js
@@ -62,96 +62,90 @@ class Cliente {
 }
 
 
-let nombredeusuario
-
-btnverClientes.addEventListener("click", (e)=>{
+btncargarCliente.addEventListener("click", (e)=>{
 
   e.preventDefault();
 
-  nombredeusuario = document.getElementById(usuarionombre.value);
-  
   listadoClientes = document.getElementById("listadodeClientes");
-  const clientes = JSON.parse(localStorage.getItem(nombredeusuario));
 
-  console.log(clientes)
-  
-//   if (clientes.length === 0){
+  if ((document.getElementById("inputnombre").value !== "") && (document.querySelector("#denominacion").value !== "") && (document.querySelector("#digitocuit").value !== "") && (document.querySelector("#mescierre").value !== "")&& (document.querySelector("#iva").value !== "")&& (document.querySelector("#ganancias").value !== "")){
 
-//     listadoClientes.innerHTML = "<h1>Por favor ingresa un nombre de usuario</h1>"
+    const cliente = new Cliente (
+
+      usuarionombre = document.getElementById("inputnombre").value, //Toma el dato ingresado en la sección anterior
+      denominacion = document.querySelector("#denominacion").value, //Variable donde guardo denominacion social del cliente
+      digitocuit = document.querySelector("#digitocuit").value, //Variable donde guardo el digito verificador de la CUIT
+      mescierre = document.querySelector("#mescierre").value, //Variable donde guardo el numero de mes de cierre de Estados Contables
+      iva = document.querySelector("#iva").value, //Variable donde guardo si esta o no inscripto en IVA
+      ganancias = document.querySelector("#ganancias").value, //Variable donde guardo si esta o no inscripto en Ganancias
+
+)
+    clientesenLS.push(cliente);
+
+    localStorage.setItem("Clientes", JSON.stringify(clientesenLS));
     
-//   }else{
-//     mostrarInfoclientes(clientes);
-//   }
-// })
-})
 
+    document.getElementById("inputnombre").value = ""
+    document.querySelector("#denominacion").value = ""
+    document.querySelector("#digitocuit").value = ""
+    document.querySelector("#mescierre").value = ""
+    document.querySelector("#iva").value = ""
+    document.querySelector("#ganancias").value = ""
 
-
-
-
-function mostrarInfoclientes(clientes) {
- let listadoClientes = document.getElementById("listadodeClientes");
-listadoClientes.innerHTML = "";
-clientes.forEach(cliente => {
-  let li = document.createElement("li");
-  li.innerHTML = `
-  <hr> ${cliente.denominacion.toUpperCase()} - IDCUIT N°: ${cliente.digitocuit} - MES CIERRE EECC: ${cliente.mescierre} - INSC. IVA: ${cliente.iva} - INSC. GCIAS.:${cliente.ganancias}`;
-
-  listadoClientes.appendChild(li);
-});
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-btncargarCliente.addEventListener("click", (e)=>{
-
-    e.preventDefault();
-
-    if ((document.getElementById("inputnombre").value !== "") && (document.querySelector("#denominacion").value !== "") && (document.querySelector("#digitocuit").value !== "") && (document.querySelector("#mescierre").value !== "")&& (document.querySelector("#iva").value !== "")&& (document.querySelector("#ganancias").value !== "")){
-
-      let cliente = new Cliente (
-        usuarionombre = document.getElementById("inputnombre").value, //Toma el dato ingresado en la sección anterior
-        denominacion = document.querySelector("#denominacion").value, //Variable donde guardo denominacion social del cliente
-        digitocuit = document.querySelector("#digitocuit").value, //Variable donde guardo el digito verificador de la CUIT
-        mescierre = document.querySelector("#mescierre").value, //Variable donde guardo el numero de mes de cierre de Estados Contables
-        iva = document.querySelector("#iva").value, //Variable donde guardo si esta o no inscripto en IVA
-        ganancias = document.querySelector("#ganancias").value, //Variable donde guardo si esta o no inscripto en Ganancias
-
-  )
-
-      listadoClientes.push(cliente)
-
-      localStorage.setItem(nombredeusuario, JSON.stringify(listadoClientes))
-
-      console.log(listadoClientes)
-
-      alert("USUARIO: " + usuarionombre + "\n" + "DATOS INGRESADOS: " + cliente.denominacion.toUpperCase() +"-"+ "Digito CUIT: " + cliente.digitocuit + "-" + "Mes Cierre: " + cliente.mescierre + "-" + "Insc. IVA: " + cliente.iva +"-" + "Insc. Gcias.: " +cliente.ganancias)
+    document.getElementById("inputnombre").focus ()
+    
       
-      document.getElementById("inputnombre").value = ""
-      document.querySelector("#denominacion").value = ""
-      document.querySelector("#digitocuit").value = ""
-      document.querySelector("#mescierre").value = ""
-      document.querySelector("#iva").value = ""
-      document.querySelector("#ganancias").value = ""
 
-      document.getElementById("inputnombre").focus ()
-    
+    alert("USUARIO: " + usuarionombre + "\n" + "DATOS INGRESADOS: " + cliente.denominacion.toUpperCase() +"-"+ "Digito CUIT: " + cliente.digitocuit + "-" + "Mes Cierre: " + cliente.mescierre + "-" + "Insc. IVA: " + cliente.iva +"-" + "Insc. Gcias.: " +cliente.ganancias)
+      
+
 
     }else{
-      alert ("POR FAVOR INGRESAR USUARIO Y DATOS CLIENTE PARA CONTINUAR")
+      listadoClientes.innerHTML = "<h1>NO PUDIMOS REGISTRAR LA INFORMACION. POR FAVOR, REVISA LOS DATOS CARGADOS</h1>";
     }
-
   })
+
+  
+
+
+btnverClientes.addEventListener("click", (e) =>{
+
+  e.preventDefault();
+
+  listadoClientes = document.getElementById("listadodeClientes");
+
+  let clientes = JSON.parse(localStorage.getItem("Clientes"));
+
+  listadoClientes.innerHTML = "";
+    
+  clientes.forEach(cliente => {
+      let li = document.createElement("li");
+      li.innerHTML = `
+      
+      <hr> ${cliente.denominacion.toUpperCase()} - IDCUIT N°: ${cliente.digitocuit} - MES CIERRE EECC: ${cliente.mescierre} - INSC. IVA: ${cliente.iva} - INSC. GCIAS.:${cliente.ganancias}`;
+    
+      listadoClientes.appendChild(li);
+    });
+  
+  
+  console.log(clientes);
+  })
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- js/variables.js
+++ js/variables.js
@@ -15,10 +15,8 @@ const btncargarCliente = document.querySelector("#btnclientes")
 
 const btnverClientes = document.querySelector("#btnverclientes")
 
-const infoclientes = [] //será un array de objetos
-
 let clientesenLS = [] //Acumula los objetos en LS
 
-let nombreUsuario = [] //Se utiliza para tomar el dato ingresado por el usuario en la sección anterior y vincularlo a sus base de datos clientes
+let NombredeUsuario = [] //Se utiliza para tomar el dato ingresado por el usuario en la sección anterior y vincularlo a sus base de datos clientes
 
 let listadoClientes =[] //Array de objetos donde se van cargando los datos de los clientes ingresados
