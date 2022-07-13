6b84e2727ef86050600a35ec3fd57d7dfff35cd4
FranCastro <franciscocast@gmail.com>
2022-07-13 09:09:20 -0300

    Repo con nueva rama 13.07

--- js/Script.js
+++ js/Script.js
@@ -1,100 +1,100 @@
-//Se definen las constantes y variables
+// //Se definen las constantes y variables
 
-const meses = [
-    { id: 1, mes: "ENERO" },
-    { id: 2, mes: "FEBRERO" },
-    { id: 3, mes: "MARZO" },
-    { id: 4, mes: "ABRIL" },
-    { id: 5, mes: "MAYO" },
-    { id: 6, mes: "JUNIO" },
-    { id: 7, mes: "JULIO" },
-    { id: 8, mes: "AGOSTO" },
-    { id: 9, mes: "SEPTIEMBRE" },
-    { id: 10, mes: "OCTUBRE" },
-    { id: 11, mes: "NOVIEMBRE" },
-    { id: 12, mes: "DICIEMBRE" },
-    { id: 13, mes: "ENERO" },
-    { id: 14, mes: "FEBRERO" },
-    { id: 15, mes: "MARZO" },
-    { id: 16, mes: "ABRIL" },
-    { id: 17, mes: "MAYO" },
-];
+// const meses = [
+//     { id: 1, mes: "ENERO" },
+//     { id: 2, mes: "FEBRERO" },
+//     { id: 3, mes: "MARZO" },
+//     { id: 4, mes: "ABRIL" },
+//     { id: 5, mes: "MAYO" },
+//     { id: 6, mes: "JUNIO" },
+//     { id: 7, mes: "JULIO" },
+//     { id: 8, mes: "AGOSTO" },
+//     { id: 9, mes: "SEPTIEMBRE" },
+//     { id: 10, mes: "OCTUBRE" },
+//     { id: 11, mes: "NOVIEMBRE" },
+//     { id: 12, mes: "DICIEMBRE" },
+//     { id: 13, mes: "ENERO" },
+//     { id: 14, mes: "FEBRERO" },
+//     { id: 15, mes: "MARZO" },
+//     { id: 16, mes: "ABRIL" },
+//     { id: 17, mes: "MAYO" },
+// ];
 
-let vtoganancias = [];
+// let vtoganancias = [];
 
-let mescierre = [];
+// let mescierre = [];
 
-let reporte = [];  
+// let reporte = [];  
 
-let denominacion = [];
-let digitocuit = [];
-let cierrecontable = [];
-let iva = [];
-let ganancias = [];
-let mesganancias = [];
-
-
-
-let nombreUsuario = [];
-let apellidoUsuario = [];
-let mailUsuario = [];
-let telUsuario = [];
-
-let datosUsuarios = [];
+// let denominacion = [];
+// let digitocuit = [];
+// let cierrecontable = [];
+// let iva = [];
+// let ganancias = [];
+// let mesganancias = [];
 
 
-//Se definen los eventos que corresponden a la primea parte del HTML
 
-//Evento que monitorea la accion del usuario sobre el botón "VERIFICAR DATOS INGRESADOS"
-let botonenvia = document.getElementById("enviadatos");
+// let nombreUsuario = [];
+// let apellidoUsuario = [];
+// let mailUsuario = [];
+// let telUsuario = [];
 
-botonenvia.addEventListener ("click", respuestaClick)
-function respuestaClick (){
-    nombreUsuario = document.getElementById("inputnombre").value;   
-    apellidoUsuario = document.getElementById("inputapellido").value;  
-    mailUsuario = document.getElementById("inputmail").value;  
-    telUsuario = document.getElementById("inputel").value;
+// let datosUsuarios = [];
+
+
+// //Se definen los eventos que corresponden a la primea parte del HTML
+
+// //Evento que monitorea la accion del usuario sobre el botón "VERIFICAR DATOS INGRESADOS"
+// let botonenvia = document.getElementById("enviadatos");
+
+// botonenvia.addEventListener ("click", respuestaClick)
+// function respuestaClick (){
+//     nombreUsuario = document.getElementById("inputnombre").value;   
+//     apellidoUsuario = document.getElementById("inputapellido").value;  
+//     mailUsuario = document.getElementById("inputmail").value;  
+//     telUsuario = document.getElementById("inputel").value;
     
-    alert("Nombre: " + nombreUsuario + " - " + "Apellido: " + apellidoUsuario + " - " + "Email: " + mailUsuario + " - " + "Teléfono: " + telUsuario + "\n");
-} 
+//     alert("Nombre: " + nombreUsuario + " - " + "Apellido: " + apellidoUsuario + " - " + "Email: " + mailUsuario + " - " + "Teléfono: " + telUsuario + "\n");
+// } 
 
 
-// ----------------------------------------------------------------------------------------
+// // ----------------------------------------------------------------------------------------
 
-//Se definen los eventos que corresponden a la segunda parte del HTML
+// //Se definen los eventos que corresponden a la segunda parte del HTML
 
 
     
-//Evento que monitorea la accion del usuario sobre el botón "RESUMEN DATOS CLIENTE"
-let botoncarga = document.getElementById("nuevacargadatos");
+// //Evento que monitorea la accion del usuario sobre el botón "RESUMEN DATOS CLIENTE"
+// let botoncarga = document.getElementById("nuevacargadatos");
 
-botoncarga.addEventListener ("click", cargadatosClick)
+// botoncarga.addEventListener ("click", cargadatosClick)
 
-function cargadatosClick (){
-    denominacion = document.getElementById("denominacion").value;
-    digitocuit = document.getElementById("digitocuit").value;
-    mescierre = Number (document.getElementById("mescierre").value);
-    iva = document.getElementById("iva").value;
-    ganancias = document.getElementById("ganancias").value;
+// function cargadatosClick (){
+//     denominacion = document.getElementById("denominacion").value;
+//     digitocuit = document.getElementById("digitocuit").value;
+//     mescierre = Number (document.getElementById("mescierre").value);
+//     iva = document.getElementById("iva").value;
+//     ganancias = document.getElementById("ganancias").value;
   
-    mescierre = meses.find((el) => el.id === (mescierre));
+//     mescierre = meses.find((el) => el.id === (mescierre));
     
-    alert("Denominacion social: " + denominacion + " - " + "Último digito CUIT: " + digitocuit + " - " + "Inscripto en IVA: " + iva + " - " + "Inscripto en Ganancias: " + ganancias + " - " + "Cierre de Ejercicio: " + mescierre.mes + "\n");
-} 
+//     alert("Denominacion social: " + denominacion + " - " + "Último digito CUIT: " + digitocuit + " - " + "Inscripto en IVA: " + iva + " - " + "Inscripto en Ganancias: " + ganancias + " - " + "Cierre de Ejercicio: " + mescierre.mes + "\n");
+// } 
 
 
 
-//Agrega elementos al HTML
+// //Agrega elementos al HTML
 
-//Agrega titulo a la primera y segunda sección del HTML
+// //Agrega titulo a la primera y segunda sección del HTML
 
-//Primera sección - ingreso datos ususario
-let titulo = document.getElementById("titulo");
-titulo.innerHTML = "<h2> Desea ingresar sus datos en nuestra base?</h2>"
+// //Primera sección - ingreso datos ususario
+// let titulo = document.getElementById("titulo");
+// titulo.innerHTML = "<h2> Desea ingresar sus datos en nuestra base?</h2>"
 
-//Segunda sección - ingreso datos empresas
-let otrotitulo = document.getElementById("otrotitulo");
-otrotitulo.innerHTML = "<h2> Desea ingresar datos de algun cliente?</h2>"
+// //Segunda sección - ingreso datos empresas
+// let otrotitulo = document.getElementById("otrotitulo");
+// otrotitulo.innerHTML = "<h2> Desea ingresar datos de algun cliente?</h2>"
 
 
 
